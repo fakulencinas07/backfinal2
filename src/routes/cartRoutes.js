2915d78c2cc6a3a9da0cce9fd8778ca68a602df7
fakulencinas07 <fakulencinas@gmail.com>
2024-10-28 23:13:11 -0300

    v3 ahora si

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import Cart from '../models/Cart.js';
 import Product from '../models/Product.js';
 import Ticket from '../models/Ticket.js';
-import User from '../models/user.js'; 
+import User from '../models/user.js';
 import { v4 as uuidv4 } from 'uuid';
 
 const router = Router();
@@ -17,7 +17,7 @@ const isAuthenticated = (req, res, next) => {
 
 // Ruta para crear un nuevo carrito
 router.post('/', isAuthenticated, async (req, res) => {
-    const userId = req.user._id; 
+    const userId = req.user._id;
 
     try {
         const newCart = new Cart({ user: userId, products: [] });
@@ -76,16 +76,16 @@ router.post('/:cid/purchase', isAuthenticated, async (req, res) => {
 
         // Verificar stock y actualizar productos disponibles
         for (const item of products) {
-            const product = await Product.findById(item.product); 
+            const product = await Product.findById(item.product);
 
-            if (product) { 
+            if (product) {
                 console.log(`Producto: ${product.name}, Stock: ${product.stock}, Cantidad: ${item.quantity}`);
-                
+
                 if (product.stock >= item.quantity) {
-                    product.stock -= item.quantity; 
-                    await product.save(); 
+                    product.stock -= item.quantity;
+                    await product.save();
                 } else {
-                    productosNoDisponibles.push(item.product); 
+                    productosNoDisponibles.push(item.product);
                 }
             } else {
                 console.log(`Producto con ID ${item.product} no encontrado.`);
@@ -102,15 +102,15 @@ router.post('/:cid/purchase', isAuthenticated, async (req, res) => {
         // Crear un ticket de compra
         const ticketAmount = await Promise.all(products.map(async item => {
             const product = await Product.findById(item.product);
-            return product && !productosNoDisponibles.includes(item.product) 
-                ? item.quantity * product.price 
-                : 0; 
+            return product && !productosNoDisponibles.includes(item.product)
+                ? item.quantity * product.price
+                : 0;
         }));
 
         const totalAmount = ticketAmount.reduce((total, amount) => total + amount, 0);
 
         const ticket = new Ticket({
-            code: uuidv4(), // Generar código único para el ticket
+            code: uuidv4(), // 
             purchase_datetime: new Date(),
             amount: totalAmount,
             purchaser: userWithCart._id
@@ -141,7 +141,7 @@ router.get('/:cid', isAuthenticated, async (req, res) => {
     const cartId = req.params.cid;
 
     try {
-        const cart = await Cart.findById(cartId).populate('products.product'); // Asegúrate de que "products.product" esté referenciado correctamente
+        const cart = await Cart.findById(cartId).populate('products.product');
         if (!cart) {
             return res.status(404).json({ message: 'Carrito no encontrado' });
         }
